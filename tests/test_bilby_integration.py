33bb0105f459edf4f0367d5def84d0cbd4eb28d9
Michael J. Williams <michaeljw1@googlemail.com>
2026-02-20 14:12:33 +0000

    TST: remove unused fixtures

--- tests/test_bilby_integration.py
+++ tests/test_bilby_integration.py
@@ -5,35 +5,6 @@ import numpy as np
 import pytest
 
 
-def model(x, m, c):
-    return m * x + c
-
-
-def conversion_func(parameters):
-    # d = |m| + |c|
-    parameters["d"] = abs(parameters["m"]) + abs(parameters["c"])
-    return parameters
-
-
-@pytest.fixture()
-def bilby_likelihood(rng):
-    x = np.linspace(0, 10, 100)
-    injection_parameters = dict(m=0.5, c=0.2)
-    sigma = 1.0
-    y = model(x, **injection_parameters) + rng.normal(0.0, sigma, len(x))
-    likelihood = bilby.likelihood.GaussianLikelihood(x, y, model, sigma)
-    return likelihood
-
-
-@pytest.fixture()
-def bilby_priors():
-    priors = bilby.core.prior.PriorDict(conversion_function=conversion_func)
-    priors["m"] = bilby.core.prior.Uniform(0, 5, boundary="periodic")
-    priors["c"] = bilby.core.prior.Uniform(-2, 2, boundary="reflective")
-    # priors["d"] = bilby.core.prior.Constraint(name="d", minimum=0, maximum=5)
-    return priors
-
-
 @pytest.fixture(params=["zuko", "flowjax"])
 def flow_backend(request):
     return request.param
